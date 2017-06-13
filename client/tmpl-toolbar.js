a1220626b5720154cb26f974065ed782af1ad7a3
David Stonikas <dstonikas@gmail.com>
2017-06-13 19:39:02 +0000

    added snippets with auto reload

---
+++ client/tmpl-toolbar.js
@@ -0,0 +1,19 @@
+Template.toolbar.events({
+  'click #btnAdd' : function(e){
+    var txtNode = $('#txtAdd');
+    if (!txtNode || !txtNode.val()) return;
+    Snippets.insert({text:txtNode.val()});
+    textNode.val('');
+  },
+  'keypress #txtAdd' : function(e){
+    if(e.keyCode!=13) return;
+    addSnippet();
+  }
+});
+
+function addSnippet(){
+var txtNode = $('#txtAdd');
+if (!txtNode || !txtNode.val()) return;
+Snippets.insert({text:txtNode.val()});
+textNode.val('');
+}
